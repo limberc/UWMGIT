b941a9848f16d7babbea0cbe225c54b5386e3253
limberc <zehua.cheng@cs.ox.ac.uk>
2022-05-15 21:33:57 +0100

    Add config.

--- configs/uwmgit_config/pspnet_unet.py
+++ configs/uwmgit_config/pspnet_unet.py
@@ -19,3 +19,21 @@ model = dict(
             type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0),
     )
 )
+total_iters = 1000
+# optimizer
+optimizer = dict(type='AdamW', lr=1e-3, betas=(0.9, 0.999), weight_decay=0.05)
+optimizer_config = dict(type='Fp16OptimizerHook', loss_scale='dynamic')
+# learning policy
+lr_config = dict(policy='poly',
+                 warmup='linear',
+                 warmup_iters=500,
+                 warmup_ratio=1e-6,
+                 power=1.0, min_lr=0.0, by_epoch=False)
+# runtime settings
+find_unused_parameters = True
+runner = dict(type='IterBasedRunner', max_iters=int(total_iters))
+checkpoint_config = dict(by_epoch=False, interval=int(total_iters), save_optimizer=False)
+evaluation = dict(by_epoch=False, interval=min(5000, int(total_iters)), metric=['imDice', 'mDice'],
+                  pre_eval=True)
+fp16 = dict()
+work_dir = f'./work_dirs/tract/baseline'
